d2191ee816ebe8c1c01eb2271f60ce4eff4fb04e
prajeesh <prajeesh@mobomo.com>
2017-11-01 14:32:09 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule }   from '@angular/forms'; // <-- NgModel lives here
+
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { BookDetailComponent } from './book-detail.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BookDetailComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpModule,
+    FormsModule // <-- import the FormsModule before binding with [(ngModel)]
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
